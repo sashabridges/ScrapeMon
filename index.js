9de931efaf95880bd080e01252c8a035ccb19f68
Sasha Bridges <sashameriebridges@gmail.com>
2022-08-11 14:08:16 -0500

    index & js & onlyrocks csv

---
+++ index.js
@@ -0,0 +1,35 @@
+function hideQ1() {
+    var x = document.getElementById("q1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function hideQ2() {
+    var x = document.getElementById("q2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function hideQ3() {
+    var x = document.getElementById("q3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function hideQ4() {
+    var x = document.getElementById("q4");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
